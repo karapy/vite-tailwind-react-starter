96c4e0b080ecffddf5d2e9766e4f578bf38455ec
Kamuran Kara <kamurankara82@gmail.com>
2024-05-28 14:55:42 +0300

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+
+  return (
+    <h1 className="text-3xl text-blue-500 font-bold underline">
+    Hello world!
+  </h1>
+  )
+}
+
+export default App
